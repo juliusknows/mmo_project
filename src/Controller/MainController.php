1315e3bbd864eb8ac3c24ba35dc29477d7fd7f95
juliusknows <ageev_m@icloud.com>
2026-02-02 04:31:37 +0300

    Update: in MainController.php
    - обновил проверки
    - поменял логику выбрасывания ошибок
    - ответ пользователю отдельно от логов.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,21 +6,27 @@ namespace App\Controller;
 
 use PDO;
 use PDOException;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
+use function strlen;
+use const FILTER_VALIDATE_EMAIL;
 use const PASSWORD_DEFAULT;
 
 final class MainController
 {
     private Environment $twig;
 
-    public function __construct(Environment $twig)
+    private LoggerInterface $logger;
+
+    public function __construct(Environment $twig, LoggerInterface $logger)
     {
         $this->twig = $twig;
+        $this->logger = $logger;
     }
 
     /**
@@ -61,31 +67,49 @@ final class MainController
         $email = (string) $request->request->get('email', '');
         $password = (string) $request->request->get('password', '');
 
+        if ('' === $name || '' === $email || '' === $password) {
+            return new Response('Все поля должны быть заполнены!', 409);
+        }
+
+        if (0 === preg_match('!^[a-zA-Z]{3,}$!', $name)) {
+            return new Response('Имя должно содержать только латиницу, быть одним словом и иметь не менее 3 символов!', 409);
+        }
+
+        if (false === filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return new Response('Некорректный Email!', 409);
+        }
+
+        if (
+            str_contains($password, ' ')
+            || str_contains($password, "\t")
+            || str_contains($password, "\n")
+            || str_contains($password, "\r")
+            || strlen($password) < 8
+            || strlen($password) > 30
+        ) {
+            return new Response('Пароль должен иметь от 8 до 30 символов и не иметь пробелов!', 409);
+        }
+
+        $stmt = $pdo->prepare('SELECT COUNT(*) FROM registration WHERE user_email = ?');
+        $stmt->execute([$email]);
+
+        if ($stmt->fetchColumn() > 0) {
+            return new Response("Email '$email' уже занят!", 409);
+        }
+
         $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
         try {
             $sql = 'INSERT INTO registration (user_name, user_email, user_password_hash) VALUES (?, ?, ?)';
             $stmt = $pdo->prepare($sql);
             $stmt->execute([$name, $email, $passwordHash]);
+            $this->logger->info("Успешная регистрация пользователя '$name'!");
 
-            return new Response('Ok! The user is registered!', 200);
+            return new Response("Пользователь '$name' успешно зарегистрирован!", 200);
         } catch (PDOException $e) {
-            $errorCode = $e->getCode();
-            $errorMessage = $e->getMessage();
+            $this->logger->error('Ошибка SQL при регистрации: ' . __FILE__ . $e->getMessage() . ' Code: ' . $e->getCode());
 
-            $errorType = '';
-
-            if ('23000' === $errorCode) {
-                if (str_contains($errorMessage, 'user_email')) {
-                    $errorType = 'The email already exists!';
-                } elseif (str_contains($errorMessage, 'user_name')) {
-                    $errorType = 'The username already exists!';
-                }
-
-                return new Response($errorType, 409);
-            }
-
-            return new Response('DB error: ' . $errorMessage, 500);
+            return new Response('Произошла ошибка при обработке запроса. Попробуйте позже.', 500);
         }
     }
 }
