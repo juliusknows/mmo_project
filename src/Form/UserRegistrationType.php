9ac48036fa4a2c2f7a5b58363779ca76e3168084
juliusknows <ageev_m@icloud.com>
2026-02-03 03:18:13 +0300

    Create: UserRegistrationDto.php & UserRegistrationType.php

---
+++ src/Form/UserRegistrationType.php
@@ -0,0 +1,44 @@
+<?php
+
+/** @noinspection PhpMultipleClassDeclarationsInspection */
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Dto\UserRegistrationDto;
+use Override;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class UserRegistrationType extends AbstractType
+{
+    #[Override]
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Пароль',
+                'required' => true,
+            ])
+            ->add('passwordRepeat', PasswordType::class, [
+                'label' => 'Подтверждение пароля',
+                'required' => true,
+            ]);
+    }
+
+    #[Override]
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserRegistrationDto::class,
+        ]);
+    }
+}
