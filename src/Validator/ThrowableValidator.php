6476ba6974a67d0139f6eed9b48ce553ab1ab262
juliusknows <ageev_m@icloud.com>
2026-02-21 21:20:41 +0300

    Fix: ThrowableValidator.php

--- src/Validator/ThrowableValidator.php
+++ src/Validator/ThrowableValidator.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Service;
+namespace App\Validator;
 
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
