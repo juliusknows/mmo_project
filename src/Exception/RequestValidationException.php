bc40361bc86a79d920170a0e1ea6ea0837fcedd0
juliusknows <ageev_m@icloud.com>
2026-02-21 21:21:28 +0300

    Create: RequestValidationException.php

---
+++ src/Exception/RequestValidationException.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use Override;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+final class RequestValidationException extends BadRequestHttpException implements ErrorDetailsContainerInterface
+{
+    private ConstraintViolationListInterface $violations;
+
+    public function __construct(ConstraintViolationListInterface $violations)
+    {
+        parent::__construct('Ошибка валидации!');
+        $this->violations = $violations;
+    }
+
+    #[Override]
+    public function getErrorDetails(): array
+    {
+        $details = [];
+
+        foreach ($this->violations as $violation) {
+            $details[$violation->getPropertyPath()][] = (string) $violation->getMessage();
+        }
+
+        return $details;
+    }
+}
