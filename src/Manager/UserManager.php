4b2c5025f197e87bc2092fa1ce19abdf5ddc4cda
juliusknows <ageev_m@icloud.com>
2026-02-25 22:35:09 +0300

    Create UserManager.php
    - менеджер для регистрации

---
+++ src/Manager/UserManager.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Manager;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+final readonly class UserManager
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function register(User $user): void
+    {
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+}
